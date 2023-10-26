35a1e06f405215e4860f70c5be6842d6294c0a11
RJLenny <robertlennon@roberts-mbp.myfiosgateway.com>
2023-10-26 00:25:10 -0400

    added logic for correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,18 +18,26 @@ button1.textContent="Start Quiz";
 
 //Add Variables for Question 1
 var q1 = "This is Question 1"
-var q1_Option1 = "This is choice 1"
-var q1_Option2 = "This is choice 2"
-var q1_Option3 = "This is choice 3"
-var q1_Option4 = "This is choice 4"
+var q1_Option1 = "This is question 1, choice 1"
+var q1_Option2 = "This is question 1, choice 2"
+var q1_Option3 = "This is question 1, choice 3"
+var q1_Option4 = "This is question 1, choice 4"
 
 //Add Variables for Question 2
+var q2 = "This is Question 2"
+var q2_Option1 = "This is question 2, choice 1"
+var q2_Option2 = "This is question 2, choice 2"
+var q2_Option3 = "This is question 2, choice 3"
+var q2_Option4 = "This is question 2, choice 4"
+
 
 pageContent.appendChild(titleEl);
 pageContent.appendChild(descriptionEl);
 pageContent.appendChild(button1);
 
 //Add event listener for the button click
+
+//*** THIS EVENT LISTENER FOR BUTTON 1 IS STILL ACTIVE AND TRYING TO REMOVE ELEMTS */
 
 button1.addEventListener("click", startQuiz = function(){
     titleEl.textContent=q1;
@@ -56,9 +64,11 @@ button1.addEventListener("click", startQuiz = function(){
 
     answerContainer.addEventListener("click",function(event){
         var element = event.target;
-        if(element.matches("button") === true) {
-            alert("You did it!!!");
-        }
+        if((element.matches("button") === true)&&(element.textContent===q1_Option4)) {
+            console.log("Correct");
+        } else if (element.matches("button")=== true) {
+            console.log("Incorrect");
+        };
     });
 
 });
